2a5aef52dbff02246b4a2adae0c6b6c041adc833
Nicole <boouko@outlook.com>
2025-02-26 15:31:10 +0800

    post added: 3S effect

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -5,7 +5,6 @@ interface SiteConfig {
 	description: string
 	lang: string
 	ogLocale: string
-	shareMessage: string
 	paginationSize: number
 }
 
@@ -16,6 +15,5 @@ export const siteConfig: SiteConfig = {
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
